e2fcd5b38223e03ce74f964541dba4fe88ef5f6d
mt5100625 <jareen.neeraj.1993@gmail.com>
2012-08-18 19:10:39 +0530

    Random Graph Generator with given instance of Nodes Created

---
+++ parser/src/New_Class.java
@@ -0,0 +1,103 @@
+import java.lang.*;
+
+import java.io.*;
+import java.util.Scanner;
+import javax.swing.JFrame;
+import prefuse.action.ActionList;
+import prefuse.action.RepaintAction;
+import prefuse.action.assignment.ColorAction;
+import prefuse.action.assignment.DataColorAction;
+import prefuse.activity.Activity;
+import prefuse.data.Graph;
+import prefuse.data.io.DataIOException;
+import prefuse.data.io.GraphMLReader;
+import prefuse.render.DefaultRendererFactory;
+import prefuse.render.LabelRenderer;
+import prefuse.util.ColorLib;
+import prefuse.visual.VisualItem;
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.geom.Rectangle2D;
+import java.util.Random;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import prefuse.Constants;
+import prefuse.Display;
+import prefuse.Visualization;
+import prefuse.action.layout.CircleLayout;
+import prefuse.action.layout.RandomLayout;
+import prefuse.action.layout.graph.ForceDirectedLayout;
+import prefuse.controls.DragControl;
+import prefuse.controls.PanControl;
+import prefuse.controls.ZoomControl;
+import prefuse.data.Graph;
+import prefuse.data.Node;
+import prefuse.data.Schema;
+import prefuse.data.Table;
+import prefuse.data.io.CSVTableReader;
+import prefuse.render.ShapeRenderer;
+import prefuse.util.FontLib;
+import prefuse.util.PrefuseLib;
+import prefuse.visual.VisualItem;
+import prefuse.visual.expression.InGroupPredicate;
+import prefuse.data.io.TableReader;
+import java.util.Random;
+
+import prefuse.data.Graph;
+import prefuse.data.Node;
+import prefuse.data.Table;
+public class New_Class 
+{
+	
+	public void create_random(Graph g)
+	{
+		Table r2 = g.getNodeTable();
+		Graph g1 = new Graph();
+		g1=g;
+		int count_neu = 0;
+		int count_right = 0;
+		int count_left = 0;
+		int n_edges = g.getEdgeCount();
+		for(int i=0;i<n_edges;i++)
+		{
+			g1.removeEdge(i);
+		}
+		int n_nodes = g1.getNodeCount();
+		Random rands = new Random();
+		for(int i=0;i<n_edges;i++)
+		{
+			int first = rands.nextInt(n_nodes);
+			int second = rands.nextInt(n_nodes);
+			while(first==second)
+			{
+				second = rands.nextInt(n_nodes);
+			}
+			g1.addEdge(first, second);
+			
+			
+			String s =   r2.getString(first,1);
+			String s2 =  r2.getString(second,1);
+			if (s.equals(s2))
+			{
+				if (s.equals("l"))
+				{
+					count_left++;
+				}
+				if (s.equals("c"))
+				{
+					count_right++;
+				}
+				if (s.equals("n"))
+				{
+					count_neu++;
+				}
+			}
+		}	
+		
+		
+	}
+
+
+
+}
