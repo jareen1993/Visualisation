a2bf8f65de837e15698de24e2ec7a4436aa75336
Keshav Goyal <gkeshav91@gmail.com>
2012-08-19 11:00:29 +0530

    Hover type effect implemented...its actualyy click effect

---
+++ parser/src/FinalControlListener.java
@@ -0,0 +1,29 @@
+
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
+
+import javax.swing.JPopupMenu;
+
+import prefuse.controls.ControlAdapter;
+import prefuse.controls.Control;
+import prefuse.visual.NodeItem;
+import prefuse.visual.VisualItem;
+
+public class FinalControlListener extends ControlAdapter implements Control {
+
+	public void itemClicked(VisualItem item, MouseEvent e) 
+	{
+		if(item instanceof NodeItem)
+		{
+			String NAME = ((String) item.get("Name"));
+			String STAND = (String) item.get("Stand");
+			
+			JPopupMenu jpub = new JPopupMenu();
+			jpub.add("Name: " + NAME);
+			jpub.add("Stand: " + STAND);
+			jpub.show(e.getComponent(),(int) item.getX(), (int) item.getY());
+		}
+	}
+
+}
